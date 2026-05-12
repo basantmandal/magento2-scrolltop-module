07d054a8a726bbf7781199c7c164c0a3c4aff4c3
Basant <basantmandal@gmail.com>
2026-05-12 07:41:53 +0000

    chore: update file permissions for multiple files in the repository

--- Model/Config/ModuleHeader.php
+++ Model/Config/ModuleHeader.php
@@ -1,35 +1,31 @@
 <?php
+declare(strict_types=1);
+
 namespace HK2\ScrollTop\Model\Config;
 
 use Magento\Config\Block\System\Config\Form\Field;
 use Magento\Framework\Data\Form\Element\AbstractElement;
-use Magento\Framework\View\Asset\Repository;
 use Magento\Framework\Module\ModuleListInterface;
 use Magento\Framework\Escaper;
 
 class ModuleHeader extends Field
 {
-    private Repository $assetRepo;
     private ModuleListInterface $moduleList;
     private Escaper $escaper;
 
     public function __construct(
         \Magento\Backend\Block\Template\Context $context,
-        Repository $assetRepo,
         ModuleListInterface $moduleList,
         Escaper $escaper,
         array $data = []
     ) {
         parent::__construct($context, $data);
-        $this->assetRepo  = $assetRepo;
         $this->moduleList = $moduleList;
         $this->escaper    = $escaper;
     }
 
     protected function _getElementHtml(AbstractElement $element): string
     {
-        $logoUrl = $this->assetRepo->getUrl('HK2_ScrollTop::images/logo.png');
-
         $module = $this->moduleList->getOne('HK2_ScrollTop');
         $version = $module['setup_version'] ?? 'N/A';
 
@@ -40,13 +36,9 @@ class ModuleHeader extends Field
         return <<<HTML
 <div class="hk2-module-header">
     <div class="hk2-header-top">
-        <img src="{$this->escaper->escapeUrl($logoUrl)}"
-             alt="HK2 ScrollTop"
-             class="hk2-logo"/>
-
         <div>
             <h2>ScrollTop</h2>
-            <p>Version {$this->escaper->escapeHtml($version)} • Magento 2 Module</p>
+            <p>Version {$this->escaper->escapeHtml($version)} &bull; Magento 2 Module</p>
         </div>
     </div>
 
--- Model/Config/SupportLink.php
+++ Model/Config/SupportLink.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace HK2\ScrollTop\Model\Config;
 
 use Magento\Config\Block\System\Config\Form\Field;
@@ -6,16 +8,16 @@ use Magento\Framework\Data\Form\Element\AbstractElement;
 
 class SupportLink extends Field
 {
-    protected function _getElementHtml(AbstractElement $element)
+    protected function _getElementHtml(AbstractElement $element): string
     {
         $supportUrl = 'https://www.basantmandal.in/support';
-        
+
         $html = '<div style="padding: 10px; background-color: #fff3cd; border: 1px solid #ffecb5; border-radius: 4px;">';
         $html .= '<strong>Support:</strong><br/>';
-        $html .= '<a href="' . $supportUrl . '" target="_blank" style="color: #664d03;">' . $supportUrl . '</a>';
+        $html .= '<a href="' . $this->escapeHtml($supportUrl) . '" target="_blank" style="color: #664d03;">' . $this->escapeHtml($supportUrl) . '</a>';
         $html .= '<p style="margin: 5px 0 0 0; color: #666; font-size: 12px;">Get help with installation, configuration, and troubleshooting</p>';
         $html .= '</div>';
-        
+
         return $html;
     }
 }
